c5769912b1135e8d941277ce008418c7615657e9
Andrew Fahrenholtz <andrew.fahrenholtz@gmail.com>
2017-09-02 22:02:44 -0500

    view the results

--- PowerMonitor/monitor/views.py
+++ PowerMonitor/monitor/views.py
@@ -1,9 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.http import HttpResponse
-
 from django.shortcuts import render
+from django.template import loader
+from models import Reading
+from pytz import timezone
+import datetime, pytz, json
+
+TIME_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
 
 def index(request):
-    return HttpResponse("Hello World")
+    # yesterday = datetime.date.today() - datetime.day
+    # readings = Reading.objects.filter(time__gte=yesterday)
+
+    db_readings = Reading.objects.order_by('time')[:1440]
+
+    readings = []
+
+    local_tz = timezone('US/Central')
+
+    for reading in db_readings:
+        local_time = reading.time.astimezone(local_tz)
+        readings.append({'time' : local_time.strftime(TIME_FORMAT), 'consumption' : reading.consumption})    
+
+    context = { 'reading_list' : json.dumps(readings) }
+
+    return render(request, 'index.html', context)
