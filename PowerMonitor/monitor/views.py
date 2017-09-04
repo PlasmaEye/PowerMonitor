aed206620029a3d5807cd26016272e0f6735e89b
Andrew Fahrenholtz <andrew.fahrenholtz@gmail.com>
2017-09-03 21:44:01 -0500

    add method for returning json data

--- PowerMonitor/monitor/views.py
+++ PowerMonitor/monitor/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.http import JsonResponse
 from monitor.models import Reading
 from pytz import timezone
 import json#, datetime
@@ -13,14 +14,29 @@ def index(request):
 
     db_readings = Reading.objects.order_by('time')[:1440]
 
-    readings = []
+    json_Readings = convert_readings_to_json(db_readings)
+
+    context = {'reading_list' : json.dumps(json_Readings)}
+
+    return render(request, 'index.html', context)
+
+def readings(request, meter_id):
+    db_readings = Reading.objects.filter(meter__id=meter_id).in_bulk()
+
+    readings_dict = convert_readings_to_dict(db_readings)
+
+    return JsonResponse(readings_dict)
+
+def convert_readings_to_json(db_readings):
+    return json.dumps(convert_readings_to_dict(db_readings))
+
+def convert_readings_to_dict(db_readings):
+    readings_dict = []
 
     local_tz = timezone('US/Central')
 
     for reading in db_readings:
         local_time = reading.time.astimezone(local_tz)
-        readings.append({'time' : local_time.strftime(ISO_TIME_FORMAT), 'consumption' : reading.consumption})    
+        readings_dict.append({'time' : local_time.strftime(ISO_TIME_FORMAT), 'consumption' : reading.consumption})
 
-    context = { 'reading_list' : json.dumps(readings) }
-
-    return render(request, 'index.html', context)
+    return readings_dict
