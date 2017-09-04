47264848024d9df1cc96734168d6f7d35bfd20b0
Andrew Fahrenholtz <andrew.fahrenholtz@gmail.com>
2017-09-04 17:09:01 -0500

    making the graph look a little better, still needs work

--- PowerMonitor/monitor/views.py
+++ PowerMonitor/monitor/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_list_or_404
 from django.http import JsonResponse
 from monitor.models import Reading, Meter
 from pytz import timezone
+from datetime import timedelta
 
 ISO_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
 
@@ -19,14 +20,25 @@ def index(request):
     return render(request, 'index.html', context)
 
 def readings(request, meter_id):
-    db_readings = get_list_or_404(Reading, meter__id=meter_id)[:1000]
+    db_readings = get_list_or_404(Reading, meter__id=meter_id)[:20]
 
-    readings_dict = convert_readings_to_dict(db_readings)
+    to_convert = []
+    time_anchor = db_readings[0].time
+    consumption_anchor = db_readings[0].consumption
+
+    for reading in db_readings:
+        time_span = reading.time - time_anchor
+        if time_span > timedelta(minutes=5):
+            to_convert.append({'time': time_anchor + timedelta(minutes=5), 'consumption': 0})
+            to_convert.append({'time': reading.time - timedelta(minutes=5), 'consumption': 0})
+
+        to_convert.append({'time': reading.time, 'consumption': reading.consumption - consumption_anchor})            
+        consumption_anchor = reading.consumption
+        time_anchor = reading.time
+
+    readings_dict = convert_readings_to_dict(to_convert)
 
     return JsonResponse(readings_dict)
-
-def convert_readings_to_json(db_readings):
-    return json.dumps(convert_readings_to_dict(db_readings))
 
 def convert_readings_to_dict(db_readings):
     readings_list = []
@@ -35,8 +47,8 @@ def convert_readings_to_dict(db_readings):
     local_tz = timezone('US/Central')
 
     for reading in db_readings:
-        local_time = reading.time.astimezone(local_tz)
+        local_time = reading['time'].astimezone(local_tz)
         readings_list.append({'time' : local_time.strftime(ISO_TIME_FORMAT), 
-                              'consumption' : reading.consumption})
+                              'consumption' : reading['consumption']})
 
     return readings_dict
