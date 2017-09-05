934628b2aa2c32d1aee0f01c2066b07005ffdd91
Andrew Fahrenholtz <andrew.fahrenholtz@gmail.com>
2017-09-04 21:13:39 -0500

    open the server to the local network

--- PowerMonitor/run.sh
+++ PowerMonitor/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 redis-server &
 celery -A PowerMonitor worker -l info -B --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-python manage.py runserver
+python manage.py runserver 0:8000
