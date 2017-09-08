27617a19863079e6e98d9625acf7bdb61c1cb3c2
Andrew Fahrenholtz <andrew.fahrenholtz@gmail.com>
2017-09-07 23:16:10 -0500

    add type descriptions

--- PowerMonitor/monitor/models.py
+++ PowerMonitor/monitor/models.py
@@ -8,6 +8,21 @@ class Meter(models.Model):
     id = models.IntegerField(default=0, primary_key=True)
     type = models.IntegerField(default=0)
 
+    electric_types = {4, 5, 7, 8}
+    gas_types = {2, 9, 12}
+    water_types = {11, 13}
+
+    def type_description(self):
+        "Returns the description of the type."
+        if(self.type in electric_types):
+            return 'Electric'
+        elif(self.type in gas_types):
+            return 'Gas'
+        elif(self.type in water_types):
+            return 'Water'
+        else:
+            return 'Unknown'
+
 class Reading(models.Model):    
     consumption = models.IntegerField(default=0)
     time = models.DateTimeField()
